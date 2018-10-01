285abbf7ad6afc19c023b07eab342c9db24877de
Brian Drennan <briansdrennan@gmail.com>
2018-10-01 13:16:32 -0700

    Setting up program

--- myDate/myDate.hpp
+++ myDate/myDate.hpp
@@ -9,6 +9,29 @@
 #ifndef myDate_hpp
 #define myDate_hpp
 
+#include <string>
 #include <stdio.h>
+
+using namespace std;
+
+class myDate{
+private:
+    int month;
+    int day;
+    int year;
+public:
+    myDate(); //default constuctor setting Date to May 11, 1959
+    myDate(int month, int day, int year); //override constructor
+    void display(); //display the date in format (Month Day, Year)
+    int daysBetween(myDate); //return positive or negative value based on future or past date
+    string dayName(); //returns Monday, Tuesday, etc...
+    void increaseDate(int num); //increase date by n days
+    void decreaseDate(int num); //decrease date by n days
+    int getMonth(); //return month
+    int getDay(); //return day
+    int getYear(); // returns year
+    int dayOfYear(); //returns the number day since year began
+    
+};
 
 #endif /* myDate_hpp */
